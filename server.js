84ee67a50f6591ec08fdba8453335fa5fa4f35ca
abu-nofal <anofal719@gmail.com>
2021-06-30 10:33:39 +0300

    fix the url img

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ class Movie{
 
     this.total_votes=data.vote_count
 
-    this.image_url=data.backdrop_path
+    this.image_url= 'https://image.tmdb.org/t/p/w300'+data.backdrop_path
 
     this.popularity=data.popularity
 
