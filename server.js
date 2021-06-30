25a6edb37e725e3a43618f183f092884c6fe874e
abu-nofal <anofal719@gmail.com>
2021-06-30 15:23:29 +0300

    class09 done

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
+'use strict'
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
-const axios = require('axios'); // require the package
-// const weatherData= require('./data/weather.json')
 require('dotenv').config();
 app.use(cors()) // after you initialize your express app instance
-
-// inside your callback function
-
+const weatherControl=require('./controller/Weather.Controller')
+const moviesControl=require('./controller/Movies.Controller')
 
 const PORT=process.env.PORT
-const WEATHER_API_KEY= process.env.WEATHER_API_KEY
-const MOVIE_API_KEY=process.env.MOVIE_API_KEY
 
 
 
@@ -22,72 +18,16 @@ app.get('/',  (req, res) =>{
 
 
 
-// ================================== for weather 
+// ================================== for weather ==================
 
-app.get('/weather', (req, res)=>{
-    let weather;
-    let lat=req.query.lat;
-    let lon=req.query.lon;
-    
-
-let url=`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
-let weatherbitResponde=axios.get(url).then(response => {
-  weather=response.data;
-  let forecast=weather.data.map(item=>{
-    return new Forecast(item)
-  })
-  // console.log(forecast)
-  res.json(forecast)
-}).catch(err=>{
-  res.status(500).send(`error in getting data ==> ${err}`)
-})
-  
-})
-
-// /movies?city=amman
-// ========================================= for movei api 
-app.get('/movies', (req, res)=>{
-   let city_name=req.query.city
-  let urlMove=`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city_name}`
-
-  let moveResponce=axios.get(urlMove).then(response => {
-    // console.log(response.data.results);
-    let movie=response.data.results.map(item=>{
-      return new Movie(item)
-    })
-    res.json(movie)
-  }).catch(err=>{
-    res.status(500).send(`error in getting data ==> ${err}`)
-})
+app.get('/weather',weatherControl )
 
 
-})
+// ================================== for movei api =======================
+app.get('/movies',moviesControl )
 
 
-
-class Forecast {
-    constructor(data){
-        this.description = data.weather.description;
-
-        this.date=data.valid_date
-       
-    }
-}
-
-class Movie{
-  constructor(data){
-    this.average_votes = data.vote_average;
-
-    this.total_votes=data.vote_count
-
-    this.image_url= 'https://image.tmdb.org/t/p/w300'+data.backdrop_path
-
-    this.popularity=data.popularity
-
-    this.released_on=data.release_date
-   
-}
-}
+//=================================== for listen 
 
 app.listen(PORT, ()=>{
   console.log('started server on port 8000')
